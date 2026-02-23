d09a5208dc6dc0f15fc7094fa112f080a1bc2dc9
Sergei Petin <petinsergeyn@yandex.ru>
2026-02-23 21:04:53 +0300

    Regenerate package-lock.json

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{padding: '20px'}}>
+      <h1>🎉 Course Platform Frontend Готов!</h1>
+      <p>npm run deploy работает!</p>
+    </div>
+  )
+}
+
+export default App
