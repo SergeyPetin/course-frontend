ce052bbe9ac492fe8e813990486cc3967e42bbde
Sergei Petin <petinsergeyn@yandex.ru>
2026-03-12 22:41:04 +0300

    fix vite cache

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        manualChunks: undefined
-      }
-    }
-  }
-})
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    host: '0.0.0.0',
+  },
+});
